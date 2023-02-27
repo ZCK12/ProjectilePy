04d0a6a483644a2804d10c9bd98bed030b1f95da
@zcol0001 <zcol0001@student.monash.edu>
2023-02-28 05:08:20 +1100

    Updated demos in the test files.

--- src/tests/projectilesimulator_tests.py
+++ src/tests/projectilesimulator_tests.py
@@ -3,7 +3,7 @@ from src.ProjectilePy.projectilesimulator import ProjectileSimulator
 import matplotlib.pyplot as plt
 import numpy as np
 
-""" DEMO 1, FINDING NEWTONIAN DRAG FIRING SOLUTIONS
+"""#DEMO 1, FINDING NEWTONIAN DRAG FIRING SOLUTIONS
 mySimulator = ProjectileSimulator(150,30,drag="Newtonian", mass=43, drag_coefficient=0.45, cross_sectional_area=0.1)
 angle1 = mySimulator.solve_angle([950, 0], False)
 angle2 = mySimulator.solve_angle([950, 0], True)
@@ -16,9 +16,10 @@ mySimulator.run(override_angle=angle2)
 x2, y2 = zip(*mySimulator.positionValues)
 ax.plot(x2, y2)
 ax.plot(x1, y1)
-"""
 
-""" DEMO 2, DEMONSTRATING THE EFFECT OF DRAG ON TRAJECTORY
+plt.title("Finding Both Firing Solutions with Drag")"""
+
+"""#DEMO 2, DEMONSTRATING THE EFFECT OF DRAG ON TRAJECTORY
 fig, ax = plt.subplots()
 
 mySimulator = ProjectileSimulator(827, 30, drag="Newtonian", mass=43, drag_coefficient=0.25, cross_sectional_area=0.0188)
@@ -35,5 +36,7 @@ mySimulator.drag = "None"
 mySimulator.run()
 x3, y3 = zip(*mySimulator.positionValues)
 ax.plot(x3, y3)
-"""
+
+plt.title("Effect of Newtonian Drag on Trajectory")"""
+
 plt.show()
